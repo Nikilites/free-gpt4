8404563528f65517d5ac5e5cb7a7cf234d33b8d9
Nikilite <nicola@acortina.it>
2024-03-27 17:29:57 +0100

    Create ai.py

---
+++ ai.py
@@ -0,0 +1,18 @@
+from g4f.client import Client
+
+client = Client()
+
+while True:
+    input_user = input("Message: ")
+
+    if input_user.lower() == 'exit':
+        break  
+
+    # server request
+    response = client.chat.completions.create(
+        model="gpt-4-turbo",
+        messages=[{"role": "user", "content": input_user}],
+        # here you can put other parameters if you need to
+    )
+
+    print("AI:", response.choices[0].message.content)
